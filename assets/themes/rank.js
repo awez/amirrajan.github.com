ae10a523db9d96f5aaa4b4b693512b24be618767
Amir Rajan <ar@amirrajan.net>
2014-03-15 14:13:47 -0500

    added overall game ranking

--- assets/themes/rank.js
+++ assets/themes/rank.js
@@ -1,13 +1,24 @@
 $.getJSON('https://itunes.apple.com/us/rss/toppaidapplications/limit=100/genre=7014/json', function(data) {
-  var rank = "< 100 T_T";
+  var rpgRank = "< 100 T_T";
+  var overrallRank = "< 200 T_T_";
 
   for(var i = 0; i < data.feed.entry.length; i++) {
     console.log(data.feed.entry[i].title.label);
     if(data.feed.entry[i].title.label == "A Dark Room - Amirali Rajan") {
-      rank = "#" + (i + 1).toString();
+      rpgRank = "#" + (i + 1).toString();
       break;
     }
   }
 
-  $("#exactrank").html("App Store RPG Ranking: " + rank);
+  $.getJSON('https://itunes.apple.com/us/rss/toppaidapplications/limit=200/genre=6014/json', function(moreData) {
+    for(var i = 0; i < moreData.feed.entry.length; i++) {
+      console.log(moreData.feed.entry[i].title.label);
+      if(moreData.feed.entry[i].title.label == "A Dark Room - Amirali Rajan") {
+        overrallRank = "#" + (i + 1).toString();
+        break;
+      }
+    }
+
+    $("#exactrank").html("App Store Ranking<br/> RPGs: " + rpgRank + "<br/>Games Overall: " + overrallRank);
+  });
 });
